4e456d9c9c6275358532919446de8d6efb80d370
SJ Martin <sjmartin271974@gmail.com>
2023-07-24 11:10:49 -0500

    Added file and instructions

---
+++ exercises/53-0/index.js
@@ -0,0 +1,31 @@
+
+//  1. Write a function that takes a string as a parameter and returns the same string in all capital letters followed by all lowercase letters
+//  capilizeAndLowercase("HelLo") // => "HELLOhello"
+
+//  2. Write a function that takes a string as a parameter and returns a number that is half the string's length, rounded down
+//  Hint ****** You'll need to use the math.floor()
+/*  findMiddleIndex("Hello") // => 2
+findMiddleIndex("Hello World") // => 5  */
+
+//  3. Write a function that uses the slice() and other functions you've written to return the first half of the given string. 
+/*
+    returnFirstHalf("Hello") // => "He"
+    returnFirstHalf("Hello World") // => "Hello"
+
+*/
+
+//  4. Write a function that takes a string as a parameter and returns that string where the first half is capitalized, and the second half is lowercasea.
+/*
+    Hint *****  if your string is odd, use math.floor() to round down
+    capilizeAndLowercase("Hello") // => "HEllo"
+    capilizeAndLowercase("Hello World") // => "HELLO world"
+
+*/
+
+// Optional Code Challange 
+
+//  This one is a step up in difficulty and utilizes the .split() string method and the .join() array method
+//  Write a function that takes a string as a parameter and capitalizes any character that follows a space.
+
+//  capitalize("hey friends! practice practice practice!") // -> "Hey Friends! Practice Practice Practice!"
+
