a473765c5c824e362788d6d9dfcc534a0d95d800
SJ Martin <sjmartin271974@gmail.com>
2023-07-24 09:46:15 -0500

    Part 1 of the exercise, successful code of the loops; some editing to do still.

--- exercises/51-5-forception/index.js
+++ exercises/51-5-forception/index.js
@@ -9,5 +9,25 @@ function forception(people, alphabet){
 ["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 */
 
-var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];       //  array of the people to be used and displayed in the for loop
+var alphabet = "abcdefghijklmnopqrstuvwxyz";                        //  array of the alphabet being useed to be displayed after every person is displayed: person then alphabet --> repeat
+var newArr = [];        // new array that the data will be pushed to and displayed at the end
+
+//  Below here is the function that is being used to loop there the people array and display each name but will display the alphabet after each name.
+// person --> alphabet --> person --> alphabet  until the loops are finished and the array's end
+forception(people, alphabet);
+
+
+function forception(people, alphabet) {
+    var newAlphabet = alphabet.split("");
+    
+
+    for (let i = 0; i < people.length; i++) {
+        newArr.push(people[i]);
+        for (let k = 0; k < newAlphabet.length; k++) {
+            newArr.push(newAlphabet[k]);
+        }
+    }
+    console.log(newArr)
+}
+
