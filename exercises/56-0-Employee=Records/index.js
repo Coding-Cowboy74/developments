f54672933020e07ff044c841b300ebeaa20fa315
SJ Martin <sjmartin271974@gmail.com>
2023-07-25 18:39:31 -0500

    Initializing the exercise and commenting the instructions

---
+++ exercises/56-0-Employee=Records/index.js
@@ -0,0 +1,22 @@
+
+/* Properties
+1. Name
+2. Job title
+3. Salary
+4. Status
+
+First create an empty array called employees
+
+Next create an Employee constructor function with the first three attributes/properties defined at the time of instantiation and the fourth will be defualted to FULL TIME
+
+The constructor function will have a method called PRINTEMPLOYEEFORM that will print the employee record to the display
+(e. g. = "Name: Bob, Title: V School Instructor, Salary: $30000, Status: Part time")
+
+1. Instantiate three employees
+
+2. Override the status attribute of one of them to either "Part time" or "Contract"
+
+3. call the PRINTEMPLOYEEFORM method for each employee 
+
+4. Enter the generated employees into the employee array
+*/
