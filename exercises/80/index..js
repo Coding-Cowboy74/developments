0c20047f0426428f6649d6d8e5b82ae201fef7ea
SJ Martin <sjmartin271974@gmail.com>
2023-10-09 10:32:57 -0500

    Updating wiht the instructions of the exercise

--- exercises/80/index..js
+++ exercises/80/index..js
@@ -1,1 +1,17 @@
 // Setting up exercise Project
+
+/*  write a function that takes an array of numbers and returns the largest (without using Math.max())
+
+// test data
+    console.log(largest([6, 13, 250, 3])) // --> 250
+    console.log(largest([3, 5, 2, 8, 1])) // --> 8
+    console.log(largest([-13, 40, 3, 0, 19, 22])) // --> 40
+
+Write a function that takes an arrary of words and a character and returns each word that has that character present.
+
+// test data
+    console.log(letterWithStrings(["$hello", "%%%%", "test!", "!"])) // --> ["$hello", "test!"]
+    console.log(letterWithStrings(["#3", "$$$", "C%4!", "Hey!", "!"])) // --> ["C%4!", "Hey!"]
+    console.log(letterWithStrings(["yellow", "green", "up", "down", "dog", "h"])) // --> []
+
+*/
