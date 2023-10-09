92eded9883adf603e303647368a8c2f6d40272b1
SJ Martin <sjmartin271974@gmail.com>
2023-10-09 10:51:02 -0500

    updating JS file with completed isDisvisible function

--- exercises/80/index.js
+++ exercises/80/index.js
@@ -14,4 +14,36 @@ Write a function that takes an arrary of words and a character and returns each 
     console.log(letterWithStrings(["#3", "$$$", "C%4!", "Hey!", "!"])) // --> ["C%4!", "Hey!"]
     console.log(letterWithStrings(["yellow", "green", "up", "down", "dog", "h"])) // --> []
 
+
+write a function that takes a num1 and num2 and returns whether num1 is divisible by num2. 
+
+// test data 
+    console.log(isDivisible(4, 2)) // --> true
+    console.log(isDivisible(9, 3)) // --> true
+    console.log(isDivisible(15, 4)) // --> false
+
 */
+
+
+
+function largest([]) {
+    
+}
+
+console.log(isDivisible(4,2));
+console.log(isDivisible(9, 3));
+console.log(isDivisible(15, 4));
+
+
+function isDivisible(num1, num2) {
+
+    if (num1 % num2 === 0) {
+        //console.log("true");
+        return true
+    }
+
+    else {
+        //console.log("false");
+        return false
+    }
+}
