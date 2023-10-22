fc1ebc9190c648ed51b766cd3610fdfaf3565ad0
SJ Martin <sjmartin271974@gmail.com>
2023-10-22 05:34:29 -0500

    successfully finished the code for the locations and diet sections.

--- exercises/Level-02/Travel-Form/index.js
+++ exercises/Level-02/Travel-Form/index.js
@@ -14,13 +14,31 @@ btnSubmit.addEventListener("click", function() {
     let age = form.custAge.value;
     let gender = form.gender.value;
 
+    let checkedLocations = [];
+    let checkedDiet = [];
+    let chkDietBoxes = document.querySelectorAll("input[name=diet]:checked");
+    let chkLocBoxes = document.querySelectorAll("input[name=locations]:checked");
+
+    for (let i = 0; i < chkLocBoxes.length; i++) {
+        checkedLocations.push(chkLocBoxes[i].value);
+    }
+
+    for (let i = 0; i < chkDietBoxes.length; i++) {
+        checkedDiet.push(chkDietBoxes[i].value);
+    }
+
     //alert(form.gender.value);
-    alert("Name: " + firstName +" " + lastName + 
+   /* alert("Name: " + firstName +" " + lastName + 
     "\n" + "Age: " + age + 
     "\n" + "Gender: " + gender +
-    "\n" + "Location: " + 
-    "\n" + "Dieatary Restrictions: ");
-    //alert(`Name: ${firstName}  ${lastName} ${gender}`);
+    "\n" + "Location: " + checkedLocations + 
+    "\n" + "Dieatary Restrictions: " + checkedDiet);
+*/
+    alert(`Name: ${firstName}  ${lastName}
+    ${"\n"}Age: ${age}
+    ${"\n"}Gender: ${gender}
+    ${"\n"}Location: ${checkedLocations}
+    ${"\n"}Dietary Requirements: ${checkedDiet}`);
 })
 
 btnClear.addEventListener("click", function() {
