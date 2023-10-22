98a3a1808b3698abd34cbe360a22c52ae499eda0
SJ Martin <sjmartin271974@gmail.com>
2023-10-22 05:09:40 -0500

    updated the JavaScript to update the fields of the fist and last name, age and gender sections. Updated the alert as well, just down to the ckboxes of location and diet

--- exercises/Level-02/Travel-Form/index.js
+++ exercises/Level-02/Travel-Form/index.js
@@ -1,6 +1,7 @@
 
 const form = document.airport; 
 const btnSubmit = document.getElementById("submit");
+const btnClear = document.getElementById("clear");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -10,6 +11,18 @@ form.addEventListener("submit", function(event) {
 btnSubmit.addEventListener("click", function() {
     let firstName = form.fName.value;
     let lastName = form.lName.value;
+    let age = form.custAge.value;
+    let gender = form.gender.value;
 
-    alert(`Name: ${firstName}  ${lastName}`);
+    //alert(form.gender.value);
+    alert("Name: " + firstName +" " + lastName + 
+    "\n" + "Age: " + age + 
+    "\n" + "Gender: " + gender +
+    "\n" + "Location: " + 
+    "\n" + "Dieatary Restrictions: ");
+    //alert(`Name: ${firstName}  ${lastName} ${gender}`);
+})
+
+btnClear.addEventListener("click", function() {
+    form.reset();
 })
