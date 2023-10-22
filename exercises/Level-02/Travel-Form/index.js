f7790b5ff46f28dd9b223adb31835fd661b9ad7d
SJ Martin <sjmartin271974@gmail.com>
2023-10-22 05:40:29 -0500

    commented the code in the files.

--- exercises/Level-02/Travel-Form/index.js
+++ exercises/Level-02/Travel-Form/index.js
@@ -9,24 +9,27 @@ form.addEventListener("submit", function(event) {
 })
 
 btnSubmit.addEventListener("click", function() {
+    //  declarations for the form and its elements 
     let firstName = form.fName.value;
     let lastName = form.lName.value;
     let age = form.custAge.value;
     let gender = form.gender.value;
 
+    //  declarations for the arrays that would be required to work with the checkboxes. 
     let checkedLocations = [];
     let checkedDiet = [];
-    let chkDietBoxes = document.querySelectorAll("input[name=diet]:checked");
-    let chkLocBoxes = document.querySelectorAll("input[name=locations]:checked");
+    let chkDietBoxes = document.querySelectorAll("input[name=diet]:checked");       //  checking to see if the check box is checked
+    let chkLocBoxes = document.querySelectorAll("input[name=locations]:checked");       // checking to see if the check box is checked 
 
     for (let i = 0; i < chkLocBoxes.length; i++) {
-        checkedLocations.push(chkLocBoxes[i].value);
+        checkedLocations.push(chkLocBoxes[i].value);        // after looping through, adding the box that are checked to the array
     }
 
     for (let i = 0; i < chkDietBoxes.length; i++) {
-        checkedDiet.push(chkDietBoxes[i].value);
+        checkedDiet.push(chkDietBoxes[i].value);            // after looping through, adding the box that are checked to the array
     }
 
+    // This first alert message is made with the ES5 way of doing it. 
     //alert(form.gender.value);
    /* alert("Name: " + firstName +" " + lastName + 
     "\n" + "Age: " + age + 
@@ -34,6 +37,7 @@ btnSubmit.addEventListener("click", function() {
     "\n" + "Location: " + checkedLocations + 
     "\n" + "Dieatary Restrictions: " + checkedDiet);
 */
+    //  Here is the ES6 way of making the alert message!
     alert(`Name: ${firstName}  ${lastName}
     ${"\n"}Age: ${age}
     ${"\n"}Gender: ${gender}
@@ -42,5 +46,5 @@ btnSubmit.addEventListener("click", function() {
 })
 
 btnClear.addEventListener("click", function() {
-    form.reset();
+    form.reset();       // Resetting the form to be cleared
 })
