61ac782e137a451dfbd1ccf6f19f1347eec8fcc1
SJ Martin <sjmartin271974@gmail.com>
2023-11-05 20:05:47 -0600

    updated the JS file and was able to get one of the sections to be working correctly.

--- exercises/Level-02/mario-pest-control/index.js
+++ exercises/Level-02/mario-pest-control/index.js
@@ -3,14 +3,32 @@ const goombaPrice = 5;
 const bobombsPrice = 7;
 const cheepCheepPrices = 11;
 
-const form = document['control-form'];
+const goomBtn = document.getElementById("calcGoombas");
+const bombsBtn = document.getElementById("calcBombs");
+const sheepsBtn = document.getElementById("calcSheeps");
+
+
+// need variables for the declarations of the 3 button controls in the form so that they can be used for the click events. 
+
+const form = document.controlForm;
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
+
+    const goombaTotal = form.goombaAmount.value;
+    console.log(goombaTotal);
+
+const goombaCoin = goombaTotal * 5;
+
+document.getElementById("goombaTotal").textContent = goombaCoin;
+
 })
+/*
+goomBtn.addEventListener("click", function() {
 
-let goombaCount = form.goombaAmount;
+    //let amountInquiry = document.goombaAmount.value;
 
-let goombaMath = parseInt(goombaCount) * parseInt(goombaPrice);
+    let calc = parseInt(form.goombaAmount.value) * parseInt(goombaPrice.textContent);
 
-let calcula
+    //console.log(goombaPrice);
+})*/
