7cb34d4dc98b3dcd2874067f86c67e0e9887a46b
SJ Martin <sjmartin271974@gmail.com>
2023-11-06 17:15:55 -0600

    updated the JS file to successfully handling of the math functions per character type.

--- exercises/Level-02/mario-pest-control/index.js
+++ exercises/Level-02/mario-pest-control/index.js
@@ -13,22 +13,34 @@ const sheepsBtn = document.getElementById("calcSheeps");
 const form = document.controlForm;
 
 form.addEventListener("submit", function (e) {
+    // Prevent the default from happening so that the form cannot update and clear all form elements
     e.preventDefault();
 
+    // In this section we are declaring the needed the information as in the amount of the goombas and then 
+    // multiplying that by the constant value at top.
     const goombaTotal = form.goombaAmount.value;
-    console.log(goombaTotal);
+    //console.log(goombaTotal);
 
-const goombaCoin = goombaTotal * 5;
+    const goombaCoin = goombaTotal * goombaPrice;       //  math part of it multiplying the 2 factors.
 
-document.getElementById("goombaTotal").textContent = goombaCoin;
+    document.getElementById("goombaTotal").textContent = goombaCoin + " coins";        // displaying the answer to the user
 
+
+    // In this section we are declaring the needed the information as in the amount of the bob-ombs and then 
+    // multiplying that by the constant value at top.
+    const bobombsTotal = form.bombsAmount.value;
+    //console.log(bobombsTotal);
+
+    const bobombsCoin = bobombsTotal * bobombsPrice;    // math part of it multiplying the 2 factors
+
+    document.getElementById("bombsTotal").textContent = bobombsCoin + " coins";        // displaying the answer to the user
+
+    // In this section we are declaring the needed the information as in the amount of the sheep-sheeps and then 
+    // multiplying that by the constant value at top.
+    const sheepGrandTotal = form.cheepAmount.value;     
+
+    const sheepCoins = sheepGrandTotal * cheepCheepPrices;  // math part of it multiplying the 2 factors
+
+    document.getElementById("sheepsTotal").textContent = sheepCoins + " coins";     // displaying the answer to the user
 })
-/*
-goomBtn.addEventListener("click", function() {
 
-    //let amountInquiry = document.goombaAmount.value;
-
-    let calc = parseInt(form.goombaAmount.value) * parseInt(goombaPrice.textContent);
-
-    //console.log(goombaPrice);
-})*/
