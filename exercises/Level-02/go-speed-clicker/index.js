57dfa571c84bac7214e6ba4fa1f5cabf5c8098f2
SJ Martin <sjmartin271974@gmail.com>
2023-11-08 11:03:48 -0600

    updated the js to make page function correctly, the css to finish the styling and the html to edit it for styling and functionality purposes.

--- exercises/Level-02/go-speed-clicker/index.js
+++ exercises/Level-02/go-speed-clicker/index.js
@@ -1,4 +1,5 @@
 const buttonsStart = document.getElementById("btnStart");
+const buttonReset = document.getElementById("btnReset");
 let ele = document.getElementById("timerClock");
 let clickCounter = 0;
 let timeLeft = 30;
@@ -6,12 +7,23 @@ let timerID = setInterval(ReverseTimer, 1000);
 
 
 buttonsStart.addEventListener("click", function() {
+  
+    clickCounter = localStorage.getItem("userClicks");
+    
 
     
     clickCounter ++;
-
-    document.getElementById("userClicks").textContent = "User Clicks: " + clickCounter;
     localStorage.setItem("userClicks", clickCounter);
+    document.getElementById("userClicks").textContent = "User Clicks: " + clickCounter;
+
+  
+    
+})
+
+buttonReset.addEventListener("click", function() {
+    clickCounter = localStorage.removeItem("userClicks");
+    document.getElementById("userClicks").textContent = `User Clicks: `;
+    timeLeft = 30;
 })
 
 function ReverseTimer() {
