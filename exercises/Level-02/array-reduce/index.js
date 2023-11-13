c9af0b3a0f40335c9a04130cf5b9307991f579c0
SJ Martin <sjmartin271974@gmail.com>
2023-11-13 12:55:42 -0600

    adding in the last 2 code challenges, num 5 almost done needs to concat the result. Num6 needs to be completed still.

--- exercises/Level-02/array-reduce/index.js
+++ exercises/Level-02/array-reduce/index.js
@@ -52,6 +52,7 @@ function totalVotes(arr) {
  //onsole.log(totalVotes(voters)); // 7
 
 
+ // 4. Given an array of all your wishlist items, figure out how much it would cose to just by everything at once.
  function shoppingSpree(arr) {
     // your code here
     const shopping = arr.reduce(function(final, spreeCosts) {
@@ -72,6 +73,60 @@ function totalVotes(arr) {
  //console.log(shoppingSpree(wishlist)); // 227005
 
 
+// 5. Given an array of arrays, flatten them into a single array
+function flatten(arr) {
+    // your code here
+    const fixingArrs = arr.reduce(function(final, current) {
+        final.push(current);
+        return final;
+    }, [])
+    return fixingArrs;
+ }
+ 
+ var arrays = [
+     ["1", "2", "3"],
+     [true],
+     [4, 5, 6]
+ ];
+ 
+ //console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
  
  
- 
+ // 6. Given an array of potential voters, return an object representing the results of the vote.
+ // Include how many of the potential votes were in AGES 18-25, how many from 26-35, how many frtom 36-55, and 
+ // how many of each of those age ranges actually voted. The resulting object containing this data should have 
+ // 6 properties. See the example output at the bottom.
+
+ var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+function voterResults(arr) {
+   // your code here
+   const result = arr.reduce(function(final, votes) {
+
+   }, {})
+   return voterResults;
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+/*
+{ numYoungVotes: 1,
+  numYoungPeople: 4,
+  numMidVotesPeople: 3,
+  numMidsPeople: 4,
+  numOldVotesPeople: 3,
+  numOldsPeople: 4
+}
+*/
