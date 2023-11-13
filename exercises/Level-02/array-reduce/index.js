abe8a8ca1994bed7361669c3e880aadd1bc0aff9
SJ Martin <sjmartin271974@gmail.com>
2023-11-13 12:28:24 -0600

    updated the JS code, 4 out of 6 code challenges completed

---
+++ exercises/Level-02/array-reduce/index.js
@@ -0,0 +1,77 @@
+
+// 1. Turn an array of numbers into a total of all the numbers 
+function total(arr) {
+    const numberTotal = arr.reduce(function(final, num) {
+        final += num;
+        return final;
+    })
+    return numberTotal;
+}
+
+//console.log(total([1, 2, 3]));
+
+
+// 2. Turn an array of numbers into a long string of all those numbers 
+function stringConcat(arr) {
+    const stringNumbers = arr.reduce(function(final, num) {
+        final.push(num);
+        return final;
+    }, [])
+    return stringNumbers;
+}
+
+//console.log(stringConcat([1, 2, 3]));
+
+
+// 3. Turn an array of voter objects into a count of how many people voted
+function totalVotes(arr) {
+    // your code here
+    const voter = arr.reduce(function(final, vote) {
+        if (vote.voted) {
+            final ++;
+        }
+        return final;
+    }, 0)
+    return voter;
+ }
+ 
+ var voters = [
+     {name:'Bob' , age: 30, voted: true},
+     {name:'Jake' , age: 32, voted: true},
+     {name:'Kate' , age: 25, voted: false},
+     {name:'Sam' , age: 20, voted: false},
+     {name:'Phil' , age: 21, voted: true},
+     {name:'Ed' , age:55, voted:true},
+     {name:'Tami' , age: 54, voted:true},
+     {name: 'Mary', age: 31, voted: false},
+     {name: 'Becky', age: 43, voted: false},
+     {name: 'Joey', age: 41, voted: true},
+     {name: 'Jeff', age: 30, voted: true},
+     {name: 'Zack', age: 19, voted: false}
+ ];
+ //onsole.log(totalVotes(voters)); // 7
+
+
+ function shoppingSpree(arr) {
+    // your code here
+    const shopping = arr.reduce(function(final, spreeCosts) {
+        final += spreeCosts.price;
+        return final;
+    }, 0)
+    return shopping;
+ }
+ 
+ var wishlist = [
+     { title: "Tesla Model S", price: 90000 },
+     { title: "4 carat diamond ring", price: 45000 },
+     { title: "Fancy hacky Sack", price: 5 },
+     { title: "Gold fidgit spinner", price: 2000 },
+     { title: "A second Tesla Model S", price: 90000 }
+ ];
+ 
+ //console.log(shoppingSpree(wishlist)); // 227005
+
+
+ 
+ 
+ 
