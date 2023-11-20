9f4636947f6f87f395c02218f55033d675a403e4
SJ Martin <sjmartin271974@gmail.com>
2023-11-20 15:49:46 -0600

    updated the 1st code part areas 1a and 1b using try and catch code blocks to handle the error handling of the code challenge.

--- exercises/Level-02/catch-me-if-you-can/index.js
+++ exercises/Level-02/catch-me-if-you-can/index.js
@@ -2,10 +2,40 @@
 // 1a) Write a function that retruns the sum of two numbers. Throw an error if either argument is not of the data type NUMBER.
 function sum(x, y){
     //check data types first and throw error
-    return x + y;
+    // if (typeof x !== "number" && typeof y !== "number") {
+    //     console.log("ERROR: Data must be in numbers, try again.");
+        
+    // }
+    // else {
+    //     return x + y;
+    // }
+
+    try {
+        if (typeof x === "number" && typeof y === "number") {
+            return x + y;
+        }
+        else {
+            throw new Error("ERROR: Data must be in numbers, try again.");
+        }
+    }
+
+    catch(err) {
+        console.log(err);
+        
+    }
   }
 
 // 1b) Call the "sum" function inside a "try" block using "1" and "2" as arguments. Use the "console.log" within a "catch" block to inform the user of the error.
+try {
+    console.log(sum(1, "2"));
+}
+
+catch(err) {
+    console.log(err);
+}
+
+
+
 
 // 2a) Given a user object, write a functin called "login" that takes a "username" and "password" as parameters. Throw an error if either of them don't mathch. Otherwise, log to the console a message saying "login successfull". 
 
