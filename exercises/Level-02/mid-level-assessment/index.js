84822a0f3102691356a659694a0d37e03d55927e
SJ Martin <sjmartin271974@gmail.com>
2023-11-20 14:41:41 -0600

    updated code for assessment with instructor

--- exercises/Level-02/mid-level-assessment/index.js
+++ exercises/Level-02/mid-level-assessment/index.js
@@ -1,33 +1,50 @@
-/*  Challenge 1: Sorting an Array
-    Write a function that takes an array of numbers and returns a new array with the numbers sorted in ascending order.
-*/
+// /*  Challenge 1: Sorting an Array
+//     Write a function that takes an array of numbers and returns a new array with the numbers sorted in ascending order.
+// */
 
-const numbers = [4, 2, 7, 1, 9, 5];
-const sortedNumbers = sortNumbers(numbers);
-console.log(sortedNumbers);
+// const numbers = [4, 2, 7, 1, 9, 5];
+// const sortedNumbers = sortNumbers(numbers);
+// console.log(sortedNumbers);
 
-function sortNumbers(numbers) {
-    // const result = numbers.sort(function(a, b) {
-    //     return a - b;
-    // })
+// function sortNumbers(numbers) {
+//     // const result = numbers.sort(function(a, b) {
+//     //     return a - b;
+//     // })
 
-    const result = numbers.sort((a, b) => a - b);
-    return result
-}
+//     const result = numbers.sort((a, b) => a - b);
+//     return result
+// }
 
-/*  Challenge 2: Mapping an Array
-    Write a function that takes an array of strings and returns a new array where each string is converted to uppercase.
-*/
+// /*  Challenge 2: Mapping an Array
+//     Write a function that takes an array of strings and returns a new array where each string is converted to uppercase.
+// */
 
-const strings = ["hello", "world", "javascript"];
-const upperCaseStrings = convertToUppercase(strings);
-console.log(upperCaseStrings);
+// const strings = ["hello", "world", "javascript"];
+// const upperCaseStrings = convertToUppercase(strings);
+// console.log(upperCaseStrings);
 
-function convertToUppercase(strings) {
-    const result = strings.map(function(stringX) {
-        return stringX.toUpperCase();
-    })
+// function convertToUppercase(strings) {
+//     const result = strings.map(function(stringX) {
+//         return stringX.toUpperCase();
+//     })
 
-    //const result = strings.map(stringX.toUpperCase());
-    return result;
-}
+//     //const result = strings.map(stringX.toUpperCase());
+//     return result;
+// }
+
+// function evenNumbers(arr) {
+//     const result = arr.filter(function(num) {
+//         return num % 2 ===0;
+//     })
+//     return result;
+// }
+
+// const evenNumbers2 = (arr) => arr.filter(num => num % 2 === 0)
+
+// const oneNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(evenNumbers(oneNumbers));
+
+
+
+
