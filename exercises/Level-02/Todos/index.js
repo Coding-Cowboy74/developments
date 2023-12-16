238ff34cb75550d279555face4c21c4336a1613d
SJ Martin <sjmartin271974@gmail.com>
2023-12-16 16:05:43 -0600

    updated JS file to finish up with displaying the required newly made elements that are all being pushed to the DOM

--- exercises/Level-02/Todos/index.js
+++ exercises/Level-02/Todos/index.js
@@ -9,7 +9,7 @@ function getData() {
 }
 
 function displayFoodList(data) {
-
+    //  Here we are creating the new elements that are going to be needed to be used for displaying purposes into the DOM object. 
     for (let i = 0; i < data.length; i++) {
         const imgs = document.createElement("img");
         const chkbx = document.createElement("input");
@@ -18,11 +18,22 @@ function displayFoodList(data) {
         const desc = document.createElement("p");
         const btnDel = document.createElement("button");
         const detailsDiv = document.createElement("div");
-        const imgDiv = document.createElement("div");
 
+        //  Here we are setting the attributes to the certian elements that need them defined. 
+        chkbx.setAttribute("type", "checkbox");
+        chkbx.checked = data[i].completed;
+
+        //  Here we are setting the text or displaying details to be shown, this is and will be based off of what the 
+        //  JSON database is saying that it will be. Being that all this data is coming from JSON. 
+        imgs.src = data[i].imgUrl;
         h2.textContent = data[i].title;
+        price.textContent = data[i].price;
+        desc.textContent = data[i].description;
+        btnDel.textContent = "X";
 
-        document.getElementById("details-side").append(h2);
+        //  Here is where all the newly created and defined elements are going to be add to the DOM for viewing purposes.
+        
+        document.getElementById("details-side").append(imgs, h2, price, desc, btnDel);
     }
 }
 getData();
