f6fbe736aade79f76d3e2dccf82bcff2c0002076
SJ Martin <sjmartin271974@gmail.com>
2024-01-06 11:13:18 -0600

    starting new project called Blogging Props

---
+++ exercises/Level-03/Bloggin-Props/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Data from './Data'
+import Navbar from './components/Navbar';
+
+function App() {
+  
+  console.log(Data);
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ exercises/Level-03/Bloggin-Props/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h3>Start Bootstrap</h3>
+            <ul>
+                <a href="#"><li>HOME</li></a>
+                <a href="#"><li>ABOUT</li></a>
+                <a href="#"><li>SAMPLE POST</li></a>
+                <a href="#"><li>CONTACT</li></a>
+            </ul>
+        </nav>
+    )
+}
