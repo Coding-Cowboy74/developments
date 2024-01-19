849e2119a8efd817a79559833f46276de99cd49d
SJ Martin <sjmartin271974@gmail.com>
2024-01-19 14:05:21 -0600

    updated the project with adding in the React App the Footer component - cannot see anything yet as only component added - no content or styles.

--- exercises/Level-03/Bloggin-Props/src/App.jsx
+++ exercises/Level-03/Bloggin-Props/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Data from './Data'
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
