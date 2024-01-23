b407cad877cafedbc367f1e315b22160b3fc9683
SJ Martin <sjmartin271974@gmail.com>
2024-01-23 11:12:32 -0600

    updated the footer with the icons from FontAwesome website. All are clickable and URL's are set too.

--- exercises/Level-03/Bloggin-Props/src/App.jsx
+++ exercises/Level-03/Bloggin-Props/src/App.jsx
@@ -4,6 +4,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import BlogList from './components/BlogList';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faFacebook, faTwitter, faGithub)
+
 function App() {
   
   console.log(Data);
--- exercises/Level-03/Bloggin-Props/src/components/Footer.jsx
+++ exercises/Level-03/Bloggin-Props/src/components/Footer.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
     return (
         <div className="border">
             <div className="container">
                 <div className="center-content">
-                    <ul>
-                        
+                    <ul className="social-media">
+                        <li><a href="https://twitter.com/" target="_blank"><FontAwesomeIcon icon="fa-brands fa-twitter" size="3x" /></a></li>
+                        <li><a href="https://www.facebook.com/" target="_blank"><FontAwesomeIcon icon="fa-brands fa-facebook" size="3x" /></a></li>
+                        <li><a href="https://github.com/" target="_blank"><FontAwesomeIcon icon="fa-brands fa-github" size="3x" /></a></li>
                     </ul>
-                    <div className="copyright-website">Copyright &copy; SJ Martin Designs 2023</div>
                 </div>
+                <div className="copyright-website">Copyright &copy; SJ Martin Designs 2023</div>
             </div>
         </div>
     )
