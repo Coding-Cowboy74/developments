3a555a06abf71da8d6bb7029e371f42dfdabc0f6
SJ Martin <sjmartin271974@gmail.com>
2024-02-29 10:39:23 -0600

    updated the css to ensure it is functioning correctly, as there were issues. Fuctioning correctly now; added in a heading and short message instructing what to do.

--- exercises/Level-03/DJ-React/src/App.jsx
+++ exercises/Level-03/DJ-React/src/App.jsx
@@ -6,7 +6,11 @@ function App() {
 
   return (
     <div>
-      
+      <h1>DJ - React</h1>
+      <p className='instruct'>
+        Feel free to test out the app by selecting any of the buttons and watch the boxes change all based on the button 
+        that you are selecting. 
+      </p>
     </div>
   )
 }
