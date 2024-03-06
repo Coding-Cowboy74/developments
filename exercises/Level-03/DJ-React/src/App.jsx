1c899968fc9a75e18869ed4c59ff30215f5da6fb
SJ Martin <sjmartin271974@gmail.com>
2024-03-05 19:17:38 -0600

    updated project moving away from the grid and just using css made boxes.

--- exercises/Level-03/DJ-React/src/App.jsx
+++ exercises/Level-03/DJ-React/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import Grid from './components/grid'
-import Buttons from './components/buttons'
+
 
 function App() {
 const colorsList = ["white", "white", "white", "white"]
@@ -13,8 +12,7 @@ const [color, setColors] = useState(colorsList)
         Feel free to test out the app by selecting any of the buttons and watch the boxes change all based on the button 
         that you are selecting. 
       </p>
-      <Grid />
-      <Buttons />
+      
       <p className='footerStyle'>
         Created by SJ Martin copyright <span className='copyright'>&copy;</span> 2024 - SJ Web Designs Inc. 
       </p>
